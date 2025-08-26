d3deb2029417112e7e73205dc8e8772be1f23687
ronish123264 <bohoraronish10@gmail.com>
2025-08-26 21:32:05 +0545

    fill remaining password characters randomly

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import random
 import string
 
 def generate_password(length):
-    character = [
+    password = [
         random.choice(string.ascii_uppercase),
         random.choice(string.ascii_lowercase),
         random.choice(string.digits),
         random.choice(string.punctuation)
     ]
     
+    character = string.ascii_uppercase + string.ascii_lowercase + string.gigits + string.punctuation
+    for _ in range(length-4):
+        password.append[random.choice(character)]
+        
+
