43d1353d35cbbaa8e8f5eac27d953b82311d6389
ronish123264 <bohoraronish10@gmail.com>
2025-08-26 21:25:31 +0545

    Ensure password contains at least 1 uppercase, 1 lowercase, 1 digit, and 1 symbol

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ import random
 import string
 
 def generate_password(length):
+    character = [
+        random.choice(string.ascii_uppercase),
+        random.choice(string.ascii_lowercase),
+        random.choice(string.digits),
+        random.choice(string.punctuation)
+    ]
+    
