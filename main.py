e79b32bc64ce636d26338ea2fb68e955ccbb493f
ronish123264 <bohoraronish10@gmail.com>
2025-08-28 18:22:44 +0545

    add the password.txt file to save the generated password

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import string
 
+
 def generate_password(length):
     password = [
         random.choice(string.ascii_uppercase),
@@ -8,12 +9,13 @@ def generate_password(length):
         random.choice(string.digits),
         random.choice(string.punctuation)
     ]
-    
-    character = string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation
+
+    character = string.ascii_uppercase + \
+        string.ascii_lowercase + string.digits + string.punctuation
     password += [random.choice(character)for _ in range(length-4)]
-    
+
     random.shuffle(password)
-    
+
     return "" .join(password)
 
 
@@ -21,9 +23,9 @@ length = int(input("Enter the length of the password (min 8): "))
 if length < 8:
     print("The length of the password must be at least 8 !!")
     exit()
-    
+
 password = generate_password(length)
 print(f"The generated password is: {password}")
 
-
-
+with open("password.txt", "a") as f:
+    f.write(password + "\n")
